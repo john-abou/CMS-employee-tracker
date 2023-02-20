d17053e22f11faaa9e30d4140f8e40273a3fe09b
John Abou <abounassar.john@gmail.com>
2023-02-20 17:48:04 -0500

    Fixed minor differnces on the server and client side. Finished the client side functionality for put requests.

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fetch = require('isomorphic-fetch');
 
 // Import prompts and functions from other files
-const {questions, addDepartment, addRole, addEmployee, updateEmployeeRole} = require("../helpers/prompt");
+const {questions, addDepartment, addRole, addEmployee, updateEmployeeRole, varCharCheck, intCheck } = require("../helpers/prompt");
 const { postFetch, getFetch, putFetch } = require("../helpers/client-requests");
 
 inquirer.prompt(questions)
@@ -20,18 +20,35 @@ inquirer.prompt(questions)
                     case "department":
                         inquirer.prompt(addDepartment)
                             .then((params) => {
+                                // Make sure the params are acceptable
+                                varCharCheck(params['department_name'], 30);
+
+                                // Make the post request
                                 postFetch(table, params);
                             });
                         break;
                     case "roles":
                         inquirer.prompt(addRole)
                             .then((params) => {
+                                // Make sure the params are acceptable
+                                varCharCheck(params['title'], 100);
+                                intCheck(params['salary']);
+                                intCheck(params['department_id']);
+
+                                // Make the post request
                                 postFetch(table, params);
                             });
                         break;
                     case "employee":
                         inquirer.prompt(addEmployee)
                             .then((params) => {
+                                // Make sure the params are acceptable
+                                varCharCheck(params['first_name'], 30);
+                                varCharCheck(params['last_name'], 30);
+                                intCheck(params['role_id']);
+                                varCharCheck(params['manager_id'], 30); 
+
+                                // Make the POST request
                                 postFetch(table, params);
                             });
                         break;
@@ -49,6 +66,12 @@ inquirer.prompt(questions)
                     case "employee":
                         inquirer.prompt(updateEmployeeRole)
                             .then((params) => {
+                                // Make sure the params are acceptable
+                                intCheck(params['role_id']);
+                                intCheck(params['manager_id']);
+                                intCheck(params['employee_id']);
+
+                                // Make the PUT request
                                 putFetch(table, params);
                             });
                         break;
