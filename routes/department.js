b7f47be850b538ed7bf032171f5280df9b1757ea
John Abou <abounassar.john@gmail.com>
2023-02-19 20:35:22 -0500

    Created all the /api/department routes

--- routes/department.js
+++ routes/department.js
@@ -4,17 +4,73 @@ const db = require('../server');
 
 // GET route for displaying the departments
 department.get('/', (req,res) => {
+    // Define the query string in a variable
+    const queryString = `SELECT * FROM department`;
 
+    // Query the database
+    db.query(queryString, (err, results) => {
+        if (err) {
+            res.json({
+                message: "error",
+                error: err
+            });
+        } else {
+            res.json({
+                message: "success",
+                data: results
+            });
+        }
+    });
 });
 
 // POST route for adding a department to the list of departments
 department.post('/', (req,res) => {
+    // Retrieve the department from the client request
+    const departmentName = req.body["depName"];
 
+    // Define the query string in a variable
+    const queryString = `INSERT INTO department (departmentName) VALUES (?)`;
+
+    // Query the database
+    db.query(queryString, departmentName, (err, results) => {
+        if (err) {
+            res.json({
+                message: "error",
+                error: err
+            });
+        } else {
+            res.json({
+                message: "success",
+                data: results
+            });
+        }
+    });
 });
 
 // Put route for updating a department's information
 department.put('/', (req,res) => {
+    // Retrieve the department from the client request
+    const departmentName = req.body["depName"];
+    const newDepartmentName = req.body["newDepName"];
+    const params = [newDepartmentName, departmentName];
 
+    // Define the query string in a variable
+    const queryString = `UPDATE department SET departmentName = ? WHERE departmentName = ?`;
+
+    // Query the database
+    db.query(queryString, params, (err, results) => {
+        if (err) {
+            res.json({
+                message: "error",
+                error: err
+            });
+        } else {
+            res.json({
+                message: "success",
+                data: results
+                });
+        }
+    });
 });
 
 // Export the roles router
