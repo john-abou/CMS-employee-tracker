4d7efdb9fddfc48969a8575aef905b0cdb0f2609
John Abou <abounassar.john@gmail.com>
2023-02-20 13:32:33 -0500

    Added helpers folder -- moved prompt and client requests to the helpers folder. Created switch logic for viewing and adding to tables on the client side.

--- public/index.js
+++ public/index.js
@@ -1,20 +1,54 @@
-// Import inquirer
+// Import modules
 const inquirer = require('inquirer');
-const cTable = require('console.table');
-const {questions, addDepartment, addRole, addEmployee, updateEmployeeRole} = require("./prompt");
 const fetch = require('isomorphic-fetch');
+
+// Import prompts and functions from other files
+const {questions, addDepartment, addRole, addEmployee, updateEmployeeRole} = require("../helpers/prompt");
+const { postFetch, getFetch } = require("../helpers/client-requests");
 
 inquirer.prompt(questions)
     .then((answers) => {
         const { action, table } = answers;
         switch (action) {
             case "view":
+                getFetch(table);
+                break;
+            case "add":
+                switch (table) {
+                    case "department":
+                        inquirer.prompt(addDepartment)
+                            .then((params) => {
+                                postFetch(table, params);
+                            });
+                        break;
+                    case "roles":
+                        inquirer.prompt(addRole)
+                            .then((params) => {
+                                postFetch(table, params);
+                            });
+                        break;
+                    case "employee":
+                        inquirer.prompt(addEmployee)
+                            .then((params) => {
+                                postFetch(table, params);
+                            });
+                        break;
+                    }
+                break;
+            case "update":
+
+
+
+
+/*
+                postFetch(table,)
                 fetch(`http://localhost:3001/api/${table}`, {
-                    method: 'GET',
+                    method: 'POST',
                     headers: {
-                      'Content-Type': 'application/json',
+                        'Content-Type': 'application/json',
                     },
-                  })
+                    body: JSON.stringify( req ),
+                })
                     .then( (response) => {
                         if (!response.ok) {
                             throw new Error('Something went wrong');
@@ -25,39 +59,8 @@ inquirer.prompt(questions)
                         const table = cTable.getTable(data['data']);
                         console.log(table);
                     });
-                    break;
-            
-            
-            
-            
-            "view all departments":
-                fetch('http://localhost:3001/api/department', {
-                    method: 'GET',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                  })
-                    .then( (response) => {
-                        if (!response.ok) {
-                            throw new Error('Something went wrong');
-                        } 
-                        return response.json();
-                        })
-                    .then( (data) => {
-                        const table = cTable.getTable(data['data']);
-                        console.log(table);
-                    });
-                    break;
-
-            case "add a department":
-
                 break;
-            case "add a role":
-
-                break;
-            case "add an employee":
-
-                break;
+                */
             case "update an employee role":
 
                 break;
