90525a87be9e444298f85e9be1b31f65e77cdb2f
John Abou <abounassar.john@gmail.com>
2023-02-21 17:43:29 -0500

    Simplified the middleware by removing the SWITCH statement.

--- middleware/cLog.js
+++ middleware/cLog.js
@@ -1,23 +1,7 @@
 // Make a function that logs the request method and the request path
 const clog = (req, res, next) => {
-    switch(req.method) {
-        case "GET": {
-            console.info(`${req.method} request made to ${req.path}`);
-            break;
-        }
-        case "POST": {
-            console.info(`${req.method} request made to ${req.path}`);
-            break;
-        }
-        case "DELETE": {
-            console.info(`${req.method} request made to ${req.path}`);
-            break;
-        }
-        default:
-            console.log(`${req.method} request made to ${req.path}`);
-            break;
-    }
-
+    console.log(`${req.method} request made to ${req.path}`);
+     
     // Call next so the server continues to the requested path after logging the request information
     next();
 };
